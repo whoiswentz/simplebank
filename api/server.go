e5a47369de176cfb27e6c55753c0b22f6317e383
Vinicios Wentz <vinicioswentz@Vinicioss-MacBook-Pro.local>
2022-04-10 22:31:13 -0300

    feat: gin installed and controllers started

---
+++ api/server.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	db "simplebank/db/sqlc"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.getAccounts)
+
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(addr string) error {
+	return server.router.Run(addr)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
--- main.go
+++ main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"database/sql"
+	"log"
+	"simplebank/api"
+	db "simplebank/db/sqlc"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgres://postgres:postgres@localhost:5432/simple_bank?sslmode=disable"
+	addr     = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	if err := server.Start(addr); err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+}
